400dd063b7fec50fab66713f33eb12d0c29baafc
nahidfarazi <nahidfarazi19@gmail.com>
2024-09-15 17:28:13 +0600

    function done

--- 9_function/function.go
+++ 9_function/function.go
@@ -1,5 +1,29 @@
 package main
 
 func main() {
-	
+	fn := func (a int) int {
+		return 2
+	}
+	processIt(fn)
+	// ADD_result := add(10,22)
+	// fmt.Println(ADD_result);
+	// SUB_result := sub(34,22)
+	// fmt.Println(SUB_result);
+	// get_value1,get_value2,get_value3 := getLang()
+	// fmt.Println(get_value1,get_value2,get_value3);
+
+}
+func getLang() (string,string,string){
+	return "golang","java","c"
+}
+
+func add(a int,b int) int{
+	return a+b
+}
+func sub(x,y int)int{
+	return x-y
+}
+
+func processIt(fn func(a int)int){
+	fn(10)
 }
