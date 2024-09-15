b50ca3af2001c08594c3530c0a34cf14ab643b84
nahidfarazi <nahidfarazi19@gmail.com>
2024-09-15 11:31:15 +0600

    variable data type done

--- variable_dataType/variable_dataType.go
+++ variable_dataType/variable_dataType.go
@@ -1,5 +1,18 @@
-package variabledatatype
+package main
+
+import "fmt"
 
 func main() {
-	
+	var varName string
+	varName = "Nahid"
+	fmt.Println(varName)
+	number := 10
+	fmt.Println(number)
+	var b bool = true
+	fmt.Println(b)
+	sum()
+}
+func sum (){
+   fmt.Println(1+2)
+
 }
