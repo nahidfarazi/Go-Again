4c836314988157536c48d77ac373eb1708e15f63
nahidfarazi <nahidfarazi19@gmail.com>
2024-09-15 12:08:59 +0600

    condition done

---
+++ 3_condition/if_else.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	if age:=11; age>=18{
+		fmt.Println("person is Adult")
+	}else if age >=12{
+		fmt.Println("person is young")
+	}else{
+		fmt.Println("Kid")
+	}
+	//condition()
+}
+
+
+/*func condition(){
+	age:=13
+	if age>18{
+		fmt.Println("person is adult")
+	}else if age >12{
+		fmt.Println("person is young")
+	}else{
+		fmt.Println("Kid")
+	}
+}*/
