6b35e1aa25803e80abcbc9d558cec8482b67df82
nahidfarazi <nahidfarazi19@gmail.com>
2024-09-15 13:13:25 +0600

    array done

---
+++ 5_arrays/arrays.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var arrNam [4] int 
+	/*array len
+	fmt.Println(len(arrNam))*/
+	/* add element in array
+	arrNam[0] = 5
+	fmt.Println(arrNam[3]);*/
+
+	/*myArray := [5] int{45,88,52,4,90}
+	fmt.Println(myArray);*/
+	// 2d array
+	towD_array := [2][2] int {{2,3},{55,1}}
+	fmt.Println(towD_array);
+}
--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 # GoLang 2025 V0.1
+
+-1.Variable
+-2.Loop
+-3.Condition
+-4.Switch
+-5.Arrays
