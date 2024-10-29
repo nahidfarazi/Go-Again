19f4cd529c32dc4de0def69c76a6b4853ccba0c1
Nahid Farazi <87297020+nahidfarazi@users.noreply.github.com>
2024-10-29 23:28:17 +0600

    Update variable_dataType.go

--- variable_dataType/variable_dataType.go
+++ variable_dataType/variable_dataType.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	
 	var varName string
 	varName = "Nahid"
 	fmt.Println(varName)
+	
 	number := 10
 	fmt.Println(number)
+
+	
 	var b bool = true
 	fmt.Println(b)
 	sum()
 }
+
 func sum (){
    fmt.Println(1+2)
 
